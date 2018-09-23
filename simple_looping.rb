c8b11eb1f6db49b7a21e82a50c02591f76da523b
Joel Rivera <joel.rivera7995@gmail.com>
2018-09-23 16:27:39 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,7 +39,8 @@ end
 
 def for_iterator(number_of_times)
 
-  phrase = "Welcome to Flatiron School's Web Development Course!"for counter in 1..7 do
-    
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  for counter in 1..7 do
+
   end
 end
